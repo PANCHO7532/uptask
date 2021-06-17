5fd694b9f5a2ee2b0ccc14e7b2d29f7c88790a40
PANCHO7532 <pancho7532@protonmail.com>
2021-06-17 11:12:06 -0300

    P7RND_155050

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const crypto = require("crypto");
+const BCrypt = require("bcrypt");
 const Users = require("../models/Users");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 //backtick: `
 exports.authenticateUser = passport.authenticate("local", {
     //xd
@@ -50,6 +53,7 @@ exports.sendToken = async(req, res) => {
     console.log(resetUrl);
 }
 exports.checkResetPassword = async(req, res) => {
+    //check if token is valid and show the prompt for let the user change the password
     const user = await Users.findOne({
         where: {
             token: req.params.token
@@ -65,5 +69,25 @@ exports.checkResetPassword = async(req, res) => {
     })
 }
 exports.updateResetPassword = async(req, res) => {
-    //TODO
+    //change password for another new one
+    const actualToken = req.params.token;
+    const user = await Users.findOne({
+        where: {
+            token: actualToken,
+            expireDate: {
+                [Op.gte]: Date.now()
+            }
+        }
+    });
+    if(!user) {
+        req.flash("error", "Not Valid");
+        res.redirect("/forgotPassword");
+    }
+    user.token = null;
+    user.expireDate = null;
+    //hash password
+    user.password = BCrypt.hashSync(req.body.password, BCrypt.genSaltSync(10));
+    await user.save();
+    req.flash("correcto", "Password changed");
+    res.redirect("/login");
 }
